e83757ed5bbea35f6cce3bd993ca2dde224bac90
MMahdi Shakouri <mm.shakourig@gmail.com>
2023-10-04 18:40:16 -0700

    Some comments added + Homogrophy

--- SIFT_app.py
+++ SIFT_app.py
@@ -5,9 +5,11 @@ from python_qt_binding import loadUi
 
 import cv2
 import sys
+import numpy as np
 
 class My_App(QtWidgets.QMainWindow):
 
+	# App Constructor
 	def __init__(self):
 		super(My_App, self).__init__()
 		loadUi("./SIFT_app.ui", self)
@@ -17,6 +19,7 @@ class My_App(QtWidgets.QMainWindow):
 		self._is_cam_enabled = False
 		self._is_template_loaded = False
 
+		# connecting the user interfaced features to the code
 		self.browse_button.clicked.connect(self.SLOT_browse_button)
 		self.toggle_cam_button.clicked.connect(self.SLOT_toggle_camera)
 
@@ -29,6 +32,9 @@ class My_App(QtWidgets.QMainWindow):
 		self._timer.timeout.connect(self.SLOT_query_camera)
 		self._timer.setInterval(int(1000 / self._cam_fps))
 
+	# Source: stackoverflow.com/questions/34232632/
+	# This function connects the browse button to a series of commands
+	# including loading the query image, running SIFT on the query image, and displaying it
 	def SLOT_browse_button(self):
 		dlg = QtWidgets.QFileDialog()
 		dlg.setFileMode(QtWidgets.QFileDialog.ExistingFile)
@@ -80,8 +86,31 @@ class My_App(QtWidgets.QMainWindow):
 
 		new_frame = cv2.drawMatches(self.query_img, self.kp_image, grayframe, kp_grayframe, good_points, grayframe)
 
-		pixmap = self.convert_cv_to_pixmap(new_frame)
-		self.live_image_label.setPixmap(pixmap)
+		pixmap = self.convert_cv_to_pixmap(frame)
+
+		if (len(good_points) > 10):
+			self._is_template_loaded = True
+			query_pts = np.float32([self.kp_image[m.queryIdx].pt for m in good_points]).reshape(-1, 1, 2)
+			train_pts = np.float32([kp_grayframe[m.trainIdx].pt for m in good_points]).reshape(-1, 1, 2)
+			matrix, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
+			matches_mask = mask.ravel().tolist()
+
+			# Perspective transform
+			h, w = self.query_img.shape[:2]
+			pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
+			dst = cv2.perspectiveTransform(pts, matrix)
+
+			# Display the homography image
+			homography = cv2.polylines(frame, [np.int32(dst)], True, (0, 0, 255), 3)
+			homography_map = self.convert_cv_to_pixmap(homography)
+
+			self.live_image_label.setPixmap(pixmap)
+			self.live_image_label.setPixmap(homography_map)
+
+		else:
+			n_frame_map = self.convert_cv_to_pixmap(new_frame)
+			self.live_image_label.setPixmap(pixmap)
+			self.live_image_label.setPixmap(n_frame_map)
 
 	def SLOT_toggle_camera(self):
 		if self._is_cam_enabled:
@@ -92,6 +121,7 @@ class My_App(QtWidgets.QMainWindow):
 			self._timer.start()
 			self._is_cam_enabled = True
 			self.toggle_cam_button.setText("&Disable camera")
+
 
 
 if __name__ == "__main__":
